caa50f4f1f0d1fd953669fd284a4d79aa3f68191
xu.quantong <xu.quantong@tlkg.com>
2019-04-23 15:47:01 +0800

    add snowflake

--- src/test/java/demo/IdWorkderDemo.java
+++ src/test/java/demo/IdWorkderDemo.java
@@ -1,10 +1,10 @@
 package demo;
 
 import com.sohu.idcenter.IdWorker;
+import com.sohu.idcenter.SnowFlakeGenerator;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 
 /**
  * @author adyliu (imxylz@gmail.com)
@@ -14,17 +14,18 @@ public class IdWorkderDemo {
 
     public static void main(String[] args) {
 
-        final long idepo = System.currentTimeMillis() - 3600 * 1000L;
-        IdWorker iw = new IdWorker(1, 1, 0, idepo);
-        IdWorker iw2 = new IdWorker(idepo);
-        for (int i = 0; i < 10; i++) {
+        final long idepo=System.currentTimeMillis() - 3600 * 1000L;
+        IdWorker iw=new IdWorker(1, 1, 0, idepo);
+        IdWorker iw2=new IdWorker(idepo);
+        for(int i=0; i < 10; i++) {
             System.out.println(iw.getId() + " -> " + iw2.getId());
         }
         System.out.println(iw);
         System.out.println(iw2);
-        long nextId = iw.getId();
+        long nextId=iw.getId();
         System.out.println(nextId);
-        long time = iw.getIdTimestamp(nextId);
-        System.out.println(time+" -> "+new SimpleDateFormat("yyyyMMddHHmmss").format(new Date(time)));
+        long time=iw.getIdTimestamp(nextId);
+        System.out.println(time + " -> " + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date(time)));
+
     }
 }
